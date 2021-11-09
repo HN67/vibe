9988536278d3b30d9f2a7156a38f1444adcaf2a3
HN67 <hn67allardkids@gmail.com>
2021-11-09 16:15:57 -0700

    Create template sql tables file

---
+++ database/tables.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Mood (
+    name nvarchar(255)
+);
+
+CREATE TABLE Taste (
+);
+
+CREATE TABLE Scent (
+);
+
+CREATE TABLE Shape (
+);
+
+CREATE TABLE Color (
+);
+
+CREATE TABLE MediaGenre (
+);
+
+CREATE TABLE MusicGenre (
+);
+
+CREATE TABLE User (
+);
+
+CREATE TABLE Admin (
+);
+
+CREATE TABLE Client (
+);
+
+CREATE TABLE Result (
+);
+
+CREATE TABLE TasteAffects (
+);
+
+CREATE TABLE ScentAffects (
+);
+
+CREATE TABLE ShapeAffects (
+);
+
+CREATE TABLE ColorAffects (
+);
+
+CREATE TABLE MediaAffects (
+);
+
+CREATE TABLE MusicAffects (
+);
