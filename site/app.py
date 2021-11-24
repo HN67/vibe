8fd7344c6b33555dda19a130f8589611a72d4011
delarasham <56743249+delarasham@users.noreply.github.com>
2021-11-23 21:44:04 -0700

    Added base html pages and css file

--- site/app.py
+++ site/app.py
@@ -6,12 +6,12 @@ import typing as t
 import flask
 
 
-def get_data(name: t.Optional[str]) -> t.Mapping[str, int]:
-    """Obtain data from *somewhere*."""
-    if name:
-        return {f"{name}_x": random.randint(1, 4), f"{name}_y": random.randint(1, 4)}
-    else:
-        return {"x": random.randint(1, 4), "y": random.randint(1, 4)}
+# def get_data(name: t.Optional[str]) -> t.Mapping[str, int]:
+#     """Obtain data from *somewhere*."""
+#     if name:
+#         return {f"{name}_x": random.randint(1, 4), f"{name}_y": random.randint(1, 4)}
+#     else:
+#         return {"x": random.randint(1, 4), "y": random.randint(1, 4)}
 
 
 def create_app() -> flask.Flask:
@@ -22,12 +22,23 @@ def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
 
     @app.route("/")
-    def _index() -> str:
-        return "<p>Hello world</p>"
+    # def _index() -> str:
+    #     return "<p>Hello world</p>"
 
-    @app.route("/data", defaults={"name": None})
-    @app.route("/data/<name>")
-    def _data(name: t.Optional[str]) -> flask.Response:
-        return flask.jsonify(get_data(name))
+    def _index() -> flask.render_template:
+        return flask.render_template("index.html")
+
+    @app.route("/profile")
+    def _profile() -> flask.render_template:
+        return flask.render_template("profile.html")
+
+    @app.route("/quiz")
+    def _quiz() -> flask.render_template:
+        return flask.render_template("quiz.html")
+
+    # @app.route("/data", defaults={"name": None})
+    # @app.route("/data/<name>")
+    # def _data(name: t.Optional[str]) -> flask.Response:
+    #     return flask.jsonify(get_data(name))
 
     return app
