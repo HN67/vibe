c3fa536f653e7848a96eda0029b89b7310181dba
HN67 <hn67allardkids@gmail.com>
2021-12-14 16:38:03 -0700

    Add full CRUD SQL for Mood

--- database/procedures.sql
+++ database/procedures.sql
@@ -2,12 +2,46 @@ USE vibe;
 
 DELIMITER //
 
--- /api/moods/
+-- GET /api/moods/
 CREATE OR REPLACE PROCEDURE get_moods()
     READS SQL DATA
     BEGIN
         SELECT (name)
-        FROM Mood;
+        FROM Mood
+        ;
+    END;
+//
+
+-- GET /api/moods/<name: string>
+CREATE OR REPLACE PROCEDURE get_mood(IN name NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM Mood
+        WHERE Mood.name = name
+        ;
+    END;
+//
+
+-- PUT /api/moods/<name: string>
+CREATE OR REPLACE PROCEDURE put_mood(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO Mood
+        VALUES (name)
+        ON DUPLICATE KEY UPDATE
+            Mood.name = name
+        ;
+    END;
+//
+
+-- DELETE /api/moods/<name: string>
+CREATE OR REPLACE PROCEDURE delete_mood(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM Mood
+        WHERE Mood.name = name
+        ;
     END;
 //
 
