1c33257c33199f299e5f37d86a4b3025d96218af
HN67 <hn67allardkids@gmail.com>
2021-12-15 15:30:19 -0700

    Replace actual procedures file

--- database/procedures.sql
+++ database/procedures.sql
@@ -2,7 +2,6 @@ USE vibe;
 
 DELIMITER //
 
--- GET /api/moods/
 CREATE OR REPLACE PROCEDURE get_moods()
     READS SQL DATA
     BEGIN
@@ -12,7 +11,6 @@ CREATE OR REPLACE PROCEDURE get_moods()
     END;
 //
 
--- GET /api/moods/<name: string>
 CREATE OR REPLACE PROCEDURE get_mood(IN name NVARCHAR(255))
     READS SQL DATA
     BEGIN
@@ -23,19 +21,17 @@ CREATE OR REPLACE PROCEDURE get_mood(IN name NVARCHAR(255))
     END;
 //
 
--- PUT /api/moods/<name: string>
 CREATE OR REPLACE PROCEDURE put_mood(IN name NVARCHAR(255))
     MODIFIES SQL DATA
     BEGIN
-        INSERT INTO Mood
+        INSERT INTO Mood (name)
         VALUES (name)
-        ON DUPLICATE KEY UPDATE
+            ON DUPLICATE KEY UPDATE
             Mood.name = name
-        ;
+;
     END;
 //
 
--- DELETE /api/moods/<name: string>
 CREATE OR REPLACE PROCEDURE delete_mood(IN name NVARCHAR(255))
     MODIFIES SQL DATA
     BEGIN
@@ -43,6 +39,318 @@ CREATE OR REPLACE PROCEDURE delete_mood(IN name NVARCHAR(255))
         WHERE Mood.name = name
         ;
     END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_tastes()
+    READS SQL DATA
+    BEGIN
+        SELECT (type)
+        FROM Taste
+        ;
+    END;
 //
+
+CREATE OR REPLACE PROCEDURE get_taste(IN type NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (type)
+        FROM Taste
+        WHERE Taste.type = type
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_taste(IN type NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO Taste (type)
+        VALUES (type)
+            ON DUPLICATE KEY UPDATE
+            Taste.type = type
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_taste(IN type NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM Taste
+        WHERE Taste.type = type
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_scents()
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM Scent
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_scent(IN name NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (name, family)
+        FROM Scent
+        WHERE Scent.name = name
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_scent(IN name NVARCHAR(255), IN family NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO Scent (name, family)
+        VALUES (name, family)
+            ON DUPLICATE KEY UPDATE
+            Scent.name = name, Scent.family = family
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_scent(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM Scent
+        WHERE Scent.name = name
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_colors()
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM Color
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_color(IN name NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (name, hue, saturation, brightness)
+        FROM Color
+        WHERE Color.name = name
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_color(IN name NVARCHAR(255), IN hue INT, IN saturation INT, IN brightness INT)
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO Color (name, hue, saturation, brightness)
+        VALUES (name, hue, saturation, brightness)
+            ON DUPLICATE KEY UPDATE
+            Color.name = name, Color.hue = hue, Color.saturation = saturation, Color.brightness = brightness
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_color(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM Color
+        WHERE Color.name = name
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_shapes()
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM Shape
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_shape(IN name NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (name, sides)
+        FROM Shape
+        WHERE Shape.name = name
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_shape(IN name NVARCHAR(255), IN sides INT)
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO Shape (name, sides)
+        VALUES (name, sides)
+            ON DUPLICATE KEY UPDATE
+            Shape.name = name, Shape.sides = sides
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_shape(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM Shape
+        WHERE Shape.name = name
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_mediagenres()
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM MediaGenre
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_mediagenre(IN name NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM MediaGenre
+        WHERE MediaGenre.name = name
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_mediagenre(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO MediaGenre (name)
+        VALUES (name)
+            ON DUPLICATE KEY UPDATE
+            MediaGenre.name = name
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_mediagenre(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM MediaGenre
+        WHERE MediaGenre.name = name
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_musicgenres()
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM MusicGenre
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_musicgenre(IN name NVARCHAR(255))
+    READS SQL DATA
+    BEGIN
+        SELECT (name)
+        FROM MusicGenre
+        WHERE MusicGenre.name = name
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_musicgenre(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO MusicGenre (name)
+        VALUES (name)
+            ON DUPLICATE KEY UPDATE
+            MusicGenre.name = name
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_musicgenre(IN name NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM MusicGenre
+        WHERE MusicGenre.name = name
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_admins()
+    READS SQL DATA
+    BEGIN
+        SELECT (id)
+        FROM admin
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_admin(IN id INT)
+    READS SQL DATA
+    BEGIN
+        SELECT (id, permissions)
+        FROM admin
+        WHERE admin.id = id
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_admin(IN id INT, IN permissions INT)
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO admin (id, permissions)
+        VALUES (id, permissions)
+            ON DUPLICATE KEY UPDATE
+            admin.id = id, admin.permissions = permissions
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_admin(IN id INT)
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM admin
+        WHERE admin.id = id
+        ;
+    END;
+//    
+
+CREATE OR REPLACE PROCEDURE get_clients()
+    READS SQL DATA
+    BEGIN
+        SELECT (id)
+        FROM client
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE get_client(IN id INT)
+    READS SQL DATA
+    BEGIN
+        SELECT (id, birthday, email, displayName, bio)
+        FROM client
+        WHERE client.id = id
+        ;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE put_client(IN id INT, IN birthday DATE, IN email NVARCHAR(255), IN displayName NVARCHAR(255), IN bio NVARCHAR(255))
+    MODIFIES SQL DATA
+    BEGIN
+        INSERT INTO client (id, birthday, email, displayName, bio)
+        VALUES (id, birthday, email, displayName, bio)
+            ON DUPLICATE KEY UPDATE
+            client.id = id, client.birthday = birthday, client.email = email, client.displayName = displayName, client.bio = bio
+;
+    END;
+//
+
+CREATE OR REPLACE PROCEDURE delete_client(IN id INT)
+    MODIFIES SQL DATA
+    BEGIN
+        DELETE FROM client
+        WHERE client.id = id
+        ;
+    END;
+//    
 
 DELIMITER ;
